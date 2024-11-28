792fef8f8763a3c34272fa8687dc8da75e9b132c
pdthang2000 <pdthang2000@gmail.com>
2024-11-28 15:34:40 +0700

    add manacher algo

--- algorithm/string/manacher_algorithm.cpp
+++ algorithm/string/manacher_algorithm.cpp
@@ -12,11 +12,12 @@ const bool MULTIPLE_QUERY = 0;
 const ll MAX_N = 200'005;
 const ll MOD = 1e9 + 7;
 
-vector<int> manacher_odd(string s) {
+void manacher_odd(string s, vector<int>& palindrome_radius, string &longest_palindrome) {
     int n = s.size();
     s = "$" + s + "^";
     vector<int> p(n + 2);
     int l = 1, r = 1;
+    int start = 0, max_len = 0, max_p = 0;
     for(int i = 1; i <= n; i++) {
         p[i] = max(0, min(r - i, p[l + (r - i)]));
         while(s[i - p[i]] == s[i + p[i]]) {
@@ -25,32 +26,38 @@ vector<int> manacher_odd(string s) {
         if(i + p[i] > r) {
             l = i - p[i], r = i + p[i];
         }
+        if (p[i] > max_p) {
+            max_p = p[i];
+            max_len = p[i] * 2 - 1;
+            start = i - p[i] + 1;
+        }
     }
-    return vector<int>(begin(p) + 1, end(p) - 1);
+    longest_palindrome = s.substr(start, max_len);
+    palindrome_radius.clear();
+    palindrome_radius = vector<int>(begin(p) + 1, end(p) - 1);
+    // p = vector<int>(begin(p) + 1, end(p) - 1);
 }
 
-vector<int> manacher(string s) {
+string manacher(string s, vector<int>& palindrome_radius, string &longest_palindrome) {
     string t;
     for(auto c: s) {
         t += string("#") + c;
     }
-    auto res = manacher_odd(t + "#");
-	vector<int> all_palindrome_center;
-	for (int i = 0; i < t.size(); ++i) {
-		if (t[i] != '#') {
-			all_palindrome_center.push_back(res[i] - 1);
-		}
-	}
-
-    return all_palindrome_center;
+    t += '#';
+    manacher_odd(t, palindrome_radius, longest_palindrome);
+    return t;
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	string s = "abcdcbazzzabcdedczz";
-	vector<int> all_palindrome_center = manacher(s);
-	for (int i = 0; i < all_palindrome_center.size(); ++i) {
-		cout << s[i] << ' ' << all_palindrome_center[i] << '\n';
+    string longest_palindrome = "";
+	vector<int> palindrome_radius;
+    string new_s = manacher(s, palindrome_radius, longest_palindrome);
+    seen(longest_palindrome);
+    seen(new_s);
+	for (int i = 0; i < palindrome_radius.size(); ++i) {
+		cout << new_s[i] << ' ' << palindrome_radius[i] << '\n';
 	}
 }
